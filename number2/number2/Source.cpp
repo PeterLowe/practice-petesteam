1a941fc75762a784197e8c7861c284de6e036421
Lowwwe <peter.lowe@itcarlow.ie>
2023-10-30 11:25:08 +0000

    even

--- number2/number2/Source.cpp
+++ number2/number2/Source.cpp
@@ -1,7 +1,16 @@
 // pete
 
+#include <iostream>
 int main()
 {
+	int number = 9;// number from user
+
+	std::cout << "the number please : ";
+	std::cin >> number;
+	if (number % 2 == 0)
+	{
+		std::cout << "number is even" << std::endl;
+	}
 
 	return 1;
 }
