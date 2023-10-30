1a854f56cdca7f79ec7a31b34168503c4faac6ed
Lowwwe <peter.lowe@itcarlow.ie>
2023-10-30 11:19:42 +0000

    odd numbers

--- number2/number2/Source.cpp
+++ number2/number2/Source.cpp
@@ -1,7 +1,17 @@
 // pete
 
+
+
+#include <iostream>
 int main()
 {
 
+	int number = 8;// user number
+	std::cout << "what's the number :";
+		std::cin >> number;
+	if (number % 2 == 1)
+	{
+		std::cout << "number is odd" << std::endl;
+	}
 	return 1;
 }
