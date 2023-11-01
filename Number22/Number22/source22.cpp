8e644ae1a3f9d98b1fce0c801682654aa767395f
Yuliia79Antonova <148494211+Yuliia79Antonova@users.noreply.github.com>
2023-11-01 23:00:00 +0000

    odd numbers

--- Number22/Number22/source22.cpp
+++ Number22/Number22/source22.cpp
@@ -1,7 +1,15 @@
 // C00301432 
 
+#include<iostream>
+
 int main()
 {
-
+	int number = 8; //user number
+	std::cout << "what's the number :";
+	std::cin >> number;
+	if (number % 2 == 1)
+	{
+		std::cout << "number is odd" << std::endl;
+	}
 	return 1;
 }
