67c821aeaab1103b93eac1be24d158925cd9d71a
Yuliia79Antonova <148494211+Yuliia79Antonova@users.noreply.github.com>
2023-11-01 23:48:27 +0000

    even

--- Number22/Number22/source22.cpp
+++ Number22/Number22/source22.cpp
@@ -1,7 +1,17 @@
 // C00301432 
 
+#include<iostream>
+
 int main()
 {
+	int number = 9; // number from user 
 
+	std::cout << "The number please :";
+	std::cin >> number; 
+
+	if (number % 2 == 0)
+	{
+		std::cout << "number is even" << std::endl;
+	}
 	return 1;
 }
