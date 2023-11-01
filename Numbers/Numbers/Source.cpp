9a76189412d4117d24bb5f97fc93cb24e6c605d7
Yuliia79Antonova <148494211+Yuliia79Antonova@users.noreply.github.com>
2023-11-01 14:46:24 +0000

    even number

--- Numbers/Numbers/Source.cpp
+++ Numbers/Numbers/Source.cpp
@@ -1,7 +1,16 @@
 // odd C00301432
 
+#include<iostream>
+
 int main()
 {
+	int number = 8; //user number
 
+	std::cout << "What is your number :";
+	std::cin >> number;
+	if (number % 2 == 0)
+	{
+		std::cout << "The number is even" << std::endl;
+	}
 	return 1;
 }
